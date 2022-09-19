c0ebeab7995a68ad8ed595c805a9936989da629c
Qaisar <syedqaisartanvir@gmail.com>
2022-09-19 17:19:55 +0500

    React beginner Practice Code!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM  from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Hello() {
+    return (
+        <span>Hello</span>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function World() {
+    return (
+        <span>World</span>
+    );
+}
+
+
+
+// Ahead of return there are braces they are not compulsory, .....
+// If you remove them make sure that the first starting tag must be ahead of return statement...
+// otherwise it will give you error.
+
+
+// if you have multiple elements in the component then wrap them using React.fragment.....
+// Because after rendering it will disappear....
+// if you use div tag then this is a block-level element.
+
+function HelloWorld() {
+    return [<Hello /> , <World />];
+}
+
+// See how react renders whitespaces.....
+
+
+function WhiteSpaces() {
+    return (
+        <div>
+            White
+            Spaces
+        </div>
+    );
+}
+
+function EmptyLines() {
+    return (
+        <div>
+            Empty
+
+            White
+
+            Lines
+        </div>
+    );
+}
+
+
+
+function Component() {
+return (
+<div className='book'>
+<div className='title'>
+The Title
+</div>
+<div className='author'>
+The Author
+</div>
+<ul className='stats'>
+<li className='rating'>
+5 stars
+</li>
+<li className='isbn'>
+12-345678-910
+</li>
+</ul>
+</div>
+)
+}
+
+function UnderTheHood() {
+return (
+    
+    React.createElement("div",{className: 'book'}, 
+        React.createElement("div", {className:"title"}, "The Author"),
+        React.createElement("ul",{className:"stats"},
+            React.createElement("li", { className:"rating" }, "5 stars"), 
+            React.createElement("li", {className: "isbn" }, "12-345678-910")))
+
+    )
+}
+
+
+function SpacesWithNewLines() {
+    return(
+        <div>
+            Non-breaking Spaces
+        </div>
+    );
+}
+
+
+function Table() {
+    return (
+            <tr>
+                <td>Item 1</td>
+                <td>Item 2</td>
+                <td>Item 3</td>
+            </tr>
+    );
+}
+
+ReactDOM.render(<Table />,document.querySelector('#root'));
